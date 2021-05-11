2b1041745ec7694cfc269c797d6822f4da048e5a
Ben Benedict <Ben.Benedict@gmail.com>
2021-05-10 20:43:30 -0500

    fixed render of index and show by code key

--- app/controllers/api/states_controller.rb
+++ app/controllers/api/states_controller.rb
@@ -1,11 +1,11 @@
 class Api::StatesController < ApplicationController
   def index
     @states = States.all
-    render json: States.All
+    render json: State.all
   end
 
   def show
-    @state = State.find_by(abbrev: params[:abbrev])
-    render json: State.find_by(abbrev: params[:abbrev])
+    @state = State.find_by(code: params[:code])
+    render json: State.find_by(code: params[:code])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/states" => "states#index"
-    get "/states/:abbrev" => "states#show"
+    get "/states/:code" => "states#show"
   end
 end
