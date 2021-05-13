f413812ded4db99257bb689097502d85e28bd7b1
Ben Benedict <Ben.Benedict@gmail.com>
2021-05-12 21:32:51 -0500

    finished index and show, and seralizer

--- app/controllers/api/states_controller.rb
+++ app/controllers/api/states_controller.rb
@@ -1,11 +1,13 @@
 class Api::StatesController < ApplicationController
   def index
-    @states = States.all
-    render json: State.all
+    @states = State.all
+    # render json: State.all
+    render "index.json.jb"
   end
 
   def show
     @state = State.find_by(code: params[:code])
-    render json: State.find_by(code: params[:code])
+    # render json: State.find_by(code: params[:code])
+    render "show.json.jb"
   end
 end
